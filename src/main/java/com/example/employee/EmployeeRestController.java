e4ba06abd6283ab509741912e2ef8db6c3f910cb
GorbunovIvan <gorbunovvivan@gmail.com>
2023-06-07 09:48:48 +0300

    non-test fixes in project

--- src/main/java/com/example/employee/EmployeeRestController.java
+++ src/main/java/com/example/employee/EmployeeRestController.java
@@ -2,12 +2,13 @@ package com.example.employee;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees/api")
+@RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeRestController {
 
@@ -19,18 +20,29 @@ public class EmployeeRestController {
     }
 
     @GetMapping("/{id}")
-    public Employee findById(@PathVariable long id) {
-        return employeeService.findById(id);
+    public ResponseEntity<Employee> findById(@PathVariable long id) {
+
+        var employee = employeeService.findById(id);
+        if (employee == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public Employee create(@RequestBody Employee employee) {
         return employeeService.create(employee);
     }
 
     @PutMapping("/{id}")
-    public Employee update(@PathVariable long id, @RequestBody Employee employee) {
-        return employeeService.update(id, employee);
+    public ResponseEntity<Employee> update(@PathVariable long id, @RequestBody Employee employee) {
+
+        var employeePersisted = employeeService.update(id, employee);
+        if (employeePersisted == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(employeePersisted, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/{id}")
